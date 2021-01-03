2bb9011d0d63f0a892dbd3f0a79d6551b9691fb9
sa02045 <sa02045@naver.com>
2021-01-03 16:12:26 +0900

    04 README.md 추가

---
+++ 04.체크박스 선택시 취소선 만들어지게 하기/README.md
@@ -0,0 +1,5 @@
+## 1. forEach 사용
+## 2. addEventListener ('change') 사용
+## 3. 삼항 연산자 사용
+## 4. js에서 css 건들이는 방법 .style
+## 5. querySelector('input[type="checkbox"]')처럼 자세하게 태그선택 가능
--- 04.체크박스 선택시 취소선 만들어지게 하기/index.js
+++ 04.체크박스 선택시 취소선 만들어지게 하기/index.js
@@ -1,4 +1,5 @@
 const liArray = document.querySelectorAll('li');
+
 liArray.forEach((li) => {
     const checkbox = li.querySelector('input[type="checkbox"]');
     const span = li.querySelector('span');
